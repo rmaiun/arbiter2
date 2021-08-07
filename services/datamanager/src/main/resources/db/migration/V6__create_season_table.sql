d3343c77022a0d633576a78aa7f0da89cd1bd9e1
Roman Maiun <roman.maiun@unicorn.com>
2021-08-07 20:15:40 +0300

    fix flyway issues

--- services/datamanager/src/main/resources/db/migration/V6__create_season_table.sql
+++ services/datamanager/src/main/resources/db/migration/V6__create_season_table.sql
@@ -5,7 +5,7 @@ create table if not exists season
     id               bigint primary key auto_increment,
     name             varchar(20) not null unique,
     algorithm        varchar(20) not null,
-    end_notification timestamp default null
+    end_notification timestamp
 );
 
 ALTER TABLE season
